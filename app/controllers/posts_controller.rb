2d03e95a581b6c38de775dc7d1ce81bc29a9cf14
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-10-27 18:42:17 +0000

    Create destroy action in comments controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,13 +13,11 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-
     if(@post.save)
       redirect_to @post
     else
       render 'new'
     end
-
   end
 
   def edit
@@ -28,19 +26,16 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
     if(@post.update(post_params))
       redirect_to @post
     else
       render 'edit'
     end
-
   end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-
     redirect_to posts_path
   end
 
